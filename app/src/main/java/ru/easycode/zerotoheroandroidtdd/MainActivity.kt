500d2871c57f100fea50cffe5a685a1aea919e73
Vladimir <okaymda@gmail.com>
2026-05-21 12:48:01 +0300

    1

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -2,10 +2,31 @@ package ru.easycode.zerotoheroandroidtdd
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.TextView
+import androidx.core.view.isVisible
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var button: Button
+
+    private lateinit var title: TextView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        title = findViewById<TextView>(R.id.countTextView)
+
+        title.text = ViewModelStub.count.toString()
+        button = findViewById(R.id.incrementButton)
+
+        button.setOnClickListener {
+            ViewModelStub.count += 2
+            title.text = ViewModelStub.count.toString()
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/ViewModelStub.kt
@@ -0,0 +1,6 @@
+package ru.easycode.zerotoheroandroidtdd
+
+object ViewModelStub {
+
+    var count = 0
+}
